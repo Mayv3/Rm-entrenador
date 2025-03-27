83195457d14423b822a261b42c8d8aba04020fe1
Nico :D <nicopereyra855@gmail.com>
2025-03-27 18:39:55 -0300

    deberia traer los pagos

--- backend/controllers/paymentsController.js
+++ backend/controllers/paymentsController.js
@@ -5,6 +5,6 @@ export const getPayments = async (req, res) => {
         await getPaymentsFromSheet(req, res);
     } catch (error) {
         console.error("Error en el controlador al obtener los pagos:", error);
-        res.status(500).json({ message: "Error al obtener los clientes" });
+        res.status(500).json({ message: "Error al obtener los pagos" });
     }
 }
