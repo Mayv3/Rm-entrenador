5288a6bc629b623638caa784aaf0f733c36d1686
Nico :D <nicopereyra855@gmail.com>
2025-03-29 07:42:12 -0300

    prueba de idSheet

--- backend/services/googleSheetsPayments.js
+++ backend/services/googleSheetsPayments.js
@@ -19,7 +19,7 @@ const auth = new google.auth.GoogleAuth({
 });
 
 const sheets = google.sheets({ version: "v4", auth });
-const SPREADSHEET_ID = "1df4bEiBYJKeTnS3QmJAvFEA6LQhJ36ZihL4mSk_KDl4";
+const SPREADSHEET_ID = "";
 const SHEET_NAME = "Hoja 1";
 
 // Pagos
